50f0ea606d49d285c1ee23d9d45a9565004f9d2c
Vikash Mishra <92555446+vm8181@users.noreply.github.com>
2025-06-19 16:02:08 +0530

    Update Search_view.sql

--- SQL Doc/Search_view.sql
+++ SQL Doc/Search_view.sql
@@ -1,16 +1,5 @@
-USE [Yogabar]
-GO
-
-/****** Object:  View [dbo].[share_of_search]    Script Date: 12-03-2025 21:18:28 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-ALTER VIEW [dbo].[share_of_search] AS
+--Search View Table
+CREATE VIEW [dbo].[share_of_search] AS
 WITH search_view AS
 (SELECT a.[date], a.keywords, a.pname, a.[asin] [code], a.price, a.prod_img, a.[rank], a.sponsered, a.[url], 'Amazon' [platform], b.brands,
 CASE WHEN b.brands LIKE '%yogabar%' OR b.brands LIKE '%yoga bar%' THEN 1 ELSE 0 END [brand_num] FROM Search a, 
@@ -23,14 +12,15 @@ SELECT s.date, s.keywords, m.Keyword_type, s.pname, s.code, s.price, s.prod_img,
 s.brand_num FROM search_view s LEFT JOIN keyword_master m ON s.keywords = m.Keywords
 GO
 
-SELECT Search.keywords, keyword_master.Keywords FROM keyword_master RIGHT JOIN Search ON keyword_master.Keywords = Search.keywords
 
+--Update keyowrd Type in raw table search from Keyword Master Table
 INSERT INTO keyword_master (Keywords, keyword_type)
 SELECT DISTINCT Search.keywords, 'generic'
 FROM Search
 LEFT JOIN keyword_master ON keyword_master.Keywords = Search.keywords
 WHERE keyword_master.Keywords IS NULL;
 
+--Updated Keyword Master Table
 UPDATE keyword_master
 SET Keyword_type = 'Generic'
 Where Keyword_type = 'generic'
